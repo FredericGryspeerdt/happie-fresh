b874abeb9d85f6b2129cfd21d99d88412d1e8e35
frederic gryspeerdt <frederic.gryspeerdt@gmail.com>
2025-12-10 16:03:50 +0100

    quick fix: add state type

--- routes/api/items.ts
+++ routes/api/items.ts
@@ -1,9 +1,9 @@
-import { FreshContext } from "fresh";
+import { Context } from "fresh";
 import { ItemRepo } from "../../database/item.repo.ts";
 
 export const handler = {
-  async POST(_ctx: FreshContext) {
-    const req = ctx.req;
+  async POST(_ctx: Context<unknown>) {
+    const req = _ctx.req;
     const item = await req.json();
     if (item.id) {
       // If an ID is provided, update the existing item
@@ -19,15 +19,15 @@ export const handler = {
     const saved = await ItemRepo.create(item);
     return new Response(JSON.stringify(saved), { status: 201 });
   },
-  async GET(_ctx: FreshContext) {
+  async GET(_ctx: Context<unknown>) {
     const items = await ItemRepo.readAll();
     return new Response(
       JSON.stringify(items),
       { status: 200, headers: { "Content-Type": "application/json" } },
     );
   },
-  async DELETE(_ctx: FreshContext) {
-    const req = ctx.req;
+  async DELETE(_ctx: Context<unknown>) {
+    const req = _ctx.req;
     const { id } = await req.json();
     if (!id) {
       return new Response("ID is required", { status: 400 });
